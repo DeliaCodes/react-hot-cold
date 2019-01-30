993d647dd7b6c4f12183b264d700dd7e8e2faa92
deliacodes <deliacodes@gmail.com>
2019-01-30 10:40:47 -0800

    Correcting props error. Test now passes.

--- src/components/status-section.test.js
+++ src/components/status-section.test.js
@@ -6,6 +6,7 @@ import StatusSection from "./status-section";
 
 describe("Renders Status Section", () => {
   it("shallow renders Status Section", () => {
-    shallow(<StatusSection />);
+    const myGuesses = [2];
+    shallow(<StatusSection guesses={myGuesses} />);
   });
 });
