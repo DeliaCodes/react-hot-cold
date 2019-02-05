abbb73c7485dee6d74e895fd294b814d86e758ca
deliacodes <me@delia.codes>
2019-02-04 19:21:41 -0800

    Adding initial actions.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// are there other actions a user can do?
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const makeGuess = guess => ({
+    type: MAKE_GUESS,
+    guess
+})
+
+export const RESET_GAME = 'RESET_GAME';
+export const resetGame = () => ({
+    type: RESET_GAME
+})
