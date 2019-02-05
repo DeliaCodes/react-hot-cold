476dd0fd2b847703ec767f519bc9ea7f1175b3e4
deliacodes <deliacodes@gmail.com>
2019-02-05 11:08:14 -0800

    editing reducer actions.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,35 @@
-import { MAKE_GUESS, RESET_GAME, SET_FEEDBACK, SET_AURAL_STATUS } from '../actions';
+import {
+  MAKE_GUESS,
+  RESET_GAME,
+  SET_FEEDBACK,
+  SET_AURAL_STATUS
+} from "../actions";
 
 const initialState = {
-    guesses: [],
-    feedback: 'Make your guess!',
-    auralStatus: '',
-    // should correctAnswer be the equation? or something else?
-    correctAnswer: Math.floor(Math.random() * 100) + 1
-}
+  guesses: [],
+  feedback: "Make your guess!",
+  auralStatus: "",
+  // should correctAnswer be the equation? or something else?
+  correctAnswer: Math.floor(Math.random() * 100) + 1
+};
 
 export const tempReducer = (state = initialState, action) => {
-    if (action.type === MAKE_GUESS) {
-        return Object.assign({}, state, {
-            guesses: [...this.state.guesses].push(action.guess)
-        });
-    }
-    else if (action.type === RESET_GAME) {
-        //is this right?
-        return state;
-    }
-    else if (action.type === SET_FEEDBACK) {
-        return Object.assign({}, state, {
-            feedback: action.feedback,
-        });
-    }
-    else if (action.type === SET_AURAL_STATUS) {
-        return Object.assign({}, state, {
-            auralStatus: action.auralStatus,
-        })
-    }
+  if (action.type === MAKE_GUESS) {
+    return Object.assign({}, state, {
+      guesses: [...state.guesses, action.guess]
+    });
+  } else if (action.type === RESET_GAME) {
+    //is this right?
+    return initialState;
+  } else if (action.type === SET_FEEDBACK) {
+    return Object.assign({}, state, {
+      feedback: action.feedback
+    });
+  } else if (action.type === SET_AURAL_STATUS) {
+    return Object.assign({}, state, {
+      auralStatus: action.auralStatus
+    });
+  }
 
-    return state;
-}
+  return state;
+};
