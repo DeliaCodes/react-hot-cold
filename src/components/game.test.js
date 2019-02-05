a219a94392de4d207089b618593b2b1a569994a3
deliacodes <deliacodes@gmail.com>
2019-02-05 11:08:35 -0800

    Updating test with Redux.

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import store from "../store";
+import { Provider } from "react-redux";
 import { shallow, mount } from "enzyme";
 
 import Game from "./game";
 
 describe("Game Test", () => {
   it("Renders Game without crashing", () => {
-    shallow(<Game />);
+    shallow(
+      <Provider store={store}>
+        <Game />
+      </Provider>
+    );
   });
 });
